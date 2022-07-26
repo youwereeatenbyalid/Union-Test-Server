499870355d2e1cbd44bd090758fefd984dc83da4
The Hitchhiker <45379459+youwereeatenbyalid@users.noreply.github.com>
2022-07-26 17:39:11 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+const hostname = '192.168.1.4';
+const crypto = require('crypto');
+// Spinning the http server and the websocket server.
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(webSocketsServerPort, hostname,() => {
+  console.log(`Server open on port ${webSocketsServerPort}/`);
+});
+
+const wsServer = new webSocketServer({
+  httpServer: server
+});
+
+connections = []
+
+let requestcounter = 0;
+
+wsServer.on("request",(request)=>{
+	console.log(`Websocket request recieved (request no. ${requestcounter} )`)
+	let connection = request.accept(null,request.origin)
+	let senderid = request.httpRequest.url.split("/")[1]
+	console.log(senderid)
+	let hasconnection = connections.find(function(connection){
+		return connection.senderid === senderid;	
+	});
+	if (typeof hasconnection !=='undefined'){
+		console.log("already connected")
+		console.log(hasconnection.senderid)
+	}else{
+		console.log("adding connection")
+		connections.push({senderid:senderid,connection:connection})
+	}
+	
+	connection.on('message', function (message){
+		let msgData = JSON.parse(message.utf8Data)
+		console.log("Recieved message: \""+msgData.message+"\"")
+		console.log(msgData)
+		let createDate = new Date()
+		console.log(createDate)
+		let responseData = {	
+			messageId:crypto.randomUUID(),
+			message:`Auto Response`,
+			senderId:4,
+			chatId:msgData.chatId,
+			date:createDate
+		}
+		console.log("Recieved message: \""+responseData.message+"\"")
+		console.log(responseData)
+		connection.send(JSON.stringify(responseData))
+		console.log("message sent")
+	});
+	requestcounter++;
+});
