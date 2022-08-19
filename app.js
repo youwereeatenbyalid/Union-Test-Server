3783c40e5e5ca4be31a96ef5cbdce71c89fde7a8
root <theadorabledeveloper@gmail.com>
2022-08-19 01:16:05 +0000

    Added basic sockets code

--- app.js
+++ app.js
@@ -43,27 +43,90 @@ import {connectToDatabase} from "./database-service.ts";
 import http from 'http';
 import https from 'https';
 import express from 'express';
+import cors from 'cors';
+const uuidv4 = require('uuid').v4;
 
-
-
+import { Server } from "socket.io";
 
 
 const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+	origin: '*',
+	methods: ['GET', 'POST']
+    }});
+
+const userConnections = new Map();
+
+class Connection {
+    constructor(io, socket) {
+	this.socket = socket;
+	this.io = io;
+
+	socket.on('getMessages', () => this.getMessages());
+	socket.on('message', (value) => this.handleMessage(value));
+	socket.on('disconnect', () => this.disconnect());
+	socket.on('connect_error', (err) => {
+	    console.log(`connect_error due to ${err.message}`);
+	});
+    }
+    
+    sendMessage(message) {
+	this.io.sockets.emit('message', message);
+    }
+    
+    getMessages() {
+	// const messages = await getMessagesAfter(req.params.address, req.params.timestamp);
+	// messages.forEach((message) => this.sendMessage(message));
+	this.io.sockets.emit('message', "this is a test");
+	
+    }
+
+    handleMessage(value) {
+	const message = {
+	    id: uuidv4(),
+	    user: this.socket,
+	    value,
+	    time: Date.now()
+	};
+	console.log(this.socket);
+
+	this.sendMessage(message);
+
+    }
+
+    disconnect() {
+	//users.delete(this.socket);
+    }
+}
+
+// function chat(io) {
+    
+// };
+
+//module.exports = chat;
+
 
 const router = express.Router();
 
 router.use(express.json());
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
+    console.log("CONNECTING");
+    console.log(io);
+    console.log(socket);
+    new Connection(io, socket);   
+});
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//   });
   
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(2999, () => {
+    console.log('listening on *:2999');
 });
 
 // address is currently phone number
@@ -239,6 +302,7 @@ connectToDatabase().then(() => {
 	console.log(`Server started at http://localhost:${443}`);
     });
     console.log("t");
+    // chat(io);
     //routerTest();
 
 });
